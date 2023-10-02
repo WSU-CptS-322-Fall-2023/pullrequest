ee04cfb33d50dc8e384965f29951e9c4072d0260
Sakire Arslan Ay <sakire.arslanay@wsu.edu>
2023-10-02 09:54:19 -0700

    commit B

---
+++ main.cc
@@ -0,0 +1,18 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int h(){
+    printf("function h");
+}
+
+int  g(int i){
+    int j = 2;
+    printf("%d\n", i+j);
+    return (i+j);
+}
+
+void main(){
+    printf("Output : %d", 12);
+    print("Done!")
+}
